f5b68fef5b9279b05ffc55a21c8d4a2a4609163f
Neous1 <Neous1@users.noreply.github.com>
2017-08-29 13:40:33 -0400

    converting string to number

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -5,20 +5,15 @@ namespace SimpleCalculator
     class Program
     {
         static void Main(string[] args)
-        {
-            string sometext = "Here is some text";
-            int number = 10;
-            double anotherNumber = 10.9;
-            bool isOrdered = true;
-            string input = Console.ReadLine();
-            var anything = 10.9;
 
-            Console.WriteLine(sometext);
-            Console.WriteLine(number);
-            Console.WriteLine(anotherNumber);
-            Console.WriteLine(isOrdered);
-            Console.WriteLine(input);
-            Console.WriteLine(anything);
+        {
+            string input = Console.ReadLine();
+
+            int convertInputToNumber;
+            int.TryParse(input, out convertInputToNumber);
+            int number = 90;
+            int result = 10 +10 - 100  +100 -number + convertInputToNumber;
+
         }
     }
 
